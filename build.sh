6ce887d35059a4df2243c3de6945bd54a3e85017
Per Thulin <per@youtify.com>
2012-06-13 18:36:33 +0200

    Remove need for background.html

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 latest_tag=$(git describe --tags)
 output_file=osd-flattr-chrome-extension-$latest_tag.zip
-zip $output_file manifest.json background.html background.js contentscript.js favicon.ico icon_48.png icon_128.png intent.html intent.js util.js
+zip $output_file manifest.json background.js contentscript.js favicon.ico icon_48.png icon_128.png intent.html intent.js util.js
 
 echo $output_file built

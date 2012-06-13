c6389cd7c79b0598d2ec0624d4517feb4daa93d9
Per Thulin <per@youtify.com>
2012-06-13 12:54:14 +0200

    Revert recursive lookups
    We decided to make the Chrome and Firefox extension the same in
    functionality, and have no plans of including recursive lookups in the
    Firefox extension.
    
    Revert "Escaping urls instead of stripping hashes, now only stripping hashes on wikipedia"
    
    This reverts commit 66482b2cca0b7d4c55dc1e428f5671e57ab8531b.
    
    Conflicts:
    	background.js
    
    Revert "adding options page which got dropped during the rebase"
    
    This reverts commit ad44fca6688c3f2b787676de9c0e8c24a335b95c.
    
    Revert "Extending flattr discovery to the wider internet"
    
    This reverts commit 914c07404832c8fd0236c5e3a5ed776a3cb9a483.
    
    Conflicts:
    	background.js
    	util.js
    
    Re-enable extension for all URLs
    
    Also support autosubmitted URLs

--- README.md
+++ README.md
@@ -3,12 +3,9 @@ Open Source Day Flattr Chrome Extension
 
 This extension for Google Chrome displays a Flattr icon in the address field when the website you are on can be flattred. Clicking the icon takes you to the thing page on flattr.com!
 
-Recursive Search
--------
-If no direct match is found, the base domain and up to 9 urls 'back' from the current URL will be searched in an attempt to find a flattr match. The extension options page allows the user turn off recursive search, control the depth of the recursion, and explains this concept in further detail.
-
 Testing
 -------
+
  1. Clone this repo.
  2. In Chrome: Tools -> Extensions.
  3. Click the + "Developer mode".
--- background.js
+++ background.js
@@ -1,83 +1,30 @@
-// This script listens for new tab events & inspects the url for flattr'ability
-//
-// If a flattr thing is found, an icon is displayed in the browser url bar,
-//
-// When the icon in address field is clicked, we open the flattr.com thing page
-// in a new tab/window.
+var url;
 
-// Global variables
-var furls = {};		// associative object containing flattr'able urls
-var debug = false;	// log a bunch of debug info to the console
-var entries = 0;	// debug var to track total # of urls checked via the API
-
-// debug logging
-function dlog(str) {
-	if (debug) {
-		console.log('entry:'+entries+' | '+str);
-	}
-}
-
-// Listen for any changes to the URL of any tab.
-// If URL exists, we show our extension icon in the address field.
+// Listen for any changes to the URL of any tab. If URL
+// exists, we show our extension icon in the address field.
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    url = undefined;
 
-	// Take no action for non http urls
-	if (tab.url.indexOf('http') != 0) {
-		dlog('non http url:'+tab.url);
-		return;
-	}
-
-	// In order to avoid duplicate requests, wait until the tab load is complete
-	// http://code.google.com/chrome/extensions/tabs.html#event-onUpdated
-	if (changeInfo.status != 'complete') {
-		dlog('non complete tab status for:'+tab.url);
-		return;
-	}
-
-	if (isWikipedia(tab.url)) {
-		// strip hashes for wikipedia urls, results in one flattr thing per page
-		furls[escape(tab.url)] =
-			createWikipediaAutoSubmitUrl(stripHashes(tab.url), tab.title);
-		chrome.pageAction.show(tabId);
-	} else {
-		entries++; // debug
-
-		// Grab retries from browser storage, defaults to 5
-		// See ./options/index.html for documentation
-		retries = localStorage['retry_times'];
-		if (isNaN(retries) || retries < -1 || retries > 9) {
-			retries = 5;
-		}
-
-		// Call the flattr API (possibly recursively) to search for tab.url
-		findFlattrThingForUrl(tab.url, retries, function(response, matchedUrl) {
-			// callback function stores the link & sets the icon in the url bar
-            var url;
-
-            if (response === false) {
-                return; // not found
+    if (isWikipedia(tab.url)) {
+        url = createWikipediaAutoSubmitUrl(tab.url, tab.title);
+        chrome.pageAction.show(tabId);
+    } else {
+        findFlattrThingForUrl(tab.url, function(thing) {
+            if (thing.message === 'flattrable') {
+                url = 'https://flattr.com/submit/auto?url=' + escape(tab.url);
+            } else if (thing) {
+                url = thing.link;
             }
-
-            if (response.message === 'flattrable') {
-                url = 'https://flattr.com/submit/auto?url=' + escape(matchedUrl);
-            } else if (response) {
-                url = response.link;
-			}
 
             if (url) {
-				furls[escape(tab.url)] = url;
-				chrome.pageAction.show(tabId);
+                chrome.pageAction.show(tabId);
             }
-		});
-	}
+        });
+    }
 });
 
-// When the icon in address field is clicked, we
-// open the flattr.com thing page in a new tab/window.
+// When the icon in address field is clicked, we open the flattr.com
+// thing page in a new tab/window.
 chrome.pageAction.onClicked.addListener(function (tab) {
-	// pull the appropriate flattr url from the global array
-	furl = furls[escape(tab.url)];
-	if (furl) {
-		window.open(furl);
-	}
+    window.open(url);
 });

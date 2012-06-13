622606072b0b53c6391fcad559dfa21ebd6a7c8c
Per Thulin <per@youtify.com>
2012-06-13 18:01:33 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+latest_tag=$(git describe --tags)
+output_file=osd-flattr-chrome-extension-$latest_tag.zip
+zip $output_file manifest.json background.html background.js contentscript.js favicon.ico icon_48.png icon_128.png intent.html intent.js util.js
+
+echo $output_file built
